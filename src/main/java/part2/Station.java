6b05f04e781cb59fa6bf71aff5669d87bf55b954
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-04-28 17:16:03 +0200

    stations encapsulation info as list of trains (arrivals or departures)

--- src/main/java/part2/Station.java
+++ src/main/java/part2/Station.java
@@ -1,2 +1,16 @@
-package part2;public interface Station {
+package part2;
+
+import java.util.List;
+
+public interface Station {
+
+    List<Train> getTrains();
+
+    int getNumberOfTrains();
+
+    boolean isArrivalsStatus();
+
+    boolean isDeparturesStatus();
+
+    String toString();
 }
--- src/main/java/part2/StationStatus.java
+++ src/main/java/part2/StationStatus.java
@@ -1,2 +1,48 @@
-package part2;public class StationImpl {
+package part2;
+
+import io.vertx.core.json.JsonArray;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class StationStatus implements Station {
+
+    public enum ARRIVALS_OR_DEPARTURES {
+        ARRIVALS, DEPARTURES
+    }
+
+    private final ARRIVALS_OR_DEPARTURES arrivalsOrDepartures;
+    private final List<Train> trains;
+
+    public StationStatus(JsonArray json, ARRIVALS_OR_DEPARTURES arrivalsOrDepartures) {
+        this.arrivalsOrDepartures = arrivalsOrDepartures;
+        this.trains = IntStream.range(0, json.size()).boxed().map(i -> new TrainStatus(json.getJsonObject(i))).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Train> getTrains() {
+        return this.trains;
+    }
+
+    @Override
+    public int getNumberOfTrains() {
+        return this.trains.size();
+    }
+
+    @Override
+    public boolean isArrivalsStatus() {
+        return this.arrivalsOrDepartures.equals(ARRIVALS_OR_DEPARTURES.ARRIVALS);
+    }
+
+    @Override
+    public boolean isDeparturesStatus() {
+        return this.arrivalsOrDepartures.equals(ARRIVALS_OR_DEPARTURES.DEPARTURES);
+    }
+
+    @Override
+    public String toString() {
+        return (isArrivalsStatus() ? "[ARRIVI]: " : "[PARTENZE] ")+getNumberOfTrains();
+    }
+
 }
