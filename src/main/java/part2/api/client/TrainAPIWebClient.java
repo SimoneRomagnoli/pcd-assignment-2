81a8e36cf09eb1e4980f567ad9c254afc913da51
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-04-29 11:34:38 +0200

    add javadoc

--- src/main/java/part2/api/client/TrainAPIWebClient.java
+++ src/main/java/part2/api/client/TrainAPIWebClient.java
@@ -24,30 +24,21 @@ public class TrainAPIWebClient {
         this.requests = new Requests(this.client);
     }
 
+    /**
+     *
+     * Method that returns up to 5 possible travel solutions,
+     * starting from an origin and a destination stations, a date and an hour of the day (0-24).
+     *
+     * @param from
+     * @param to
+     * @param startingDate
+     * @param startingTime
+     * @return a list of travels
+     */
     public Future<List<Travel>> getTrainSolutions(String from, String to, String startingDate, int startingTime) {
         try {
             Promise<JsonArray> jsonResponse = this.requests.getRequestForTravelSolutions(from, to, startingDate, startingTime);
             return futureTravelSolutions(jsonResponse);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public Future<Train> getRealTimeTrainInfo(String stationName, String trainCode) {
-        try {
-            Promise<JsonObject> jsonResponse = this.requests.getRequestForRealTimeTrainInfo(stationName, trainCode, System.currentTimeMillis());
-            return futureRealTimeTrainInfo(jsonResponse);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public Future<Station> getRealTimeStationInfo(String stationName, StationStatus.ArrivalsOrDepartures arrivalsOrDepartures) {
-        try {
-            Promise<JsonArray> jsonResponse = this.requests.getRequestForRealTimeStationInfo(stationName, arrivalsOrDepartures, Calendar.getInstance().getTime());
-            return futureRealTimeStationInfo(jsonResponse, arrivalsOrDepartures);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,10 +53,6 @@ public class TrainAPIWebClient {
                     System.out.println("Something went wrong in JSON response: " + err.getMessage());
                     travels.fail(err.getMessage());
                 })
-                .onSuccess(jsonArray -> {
-                    //System.out.println("Received a json array: " + jsonArray);
-                    /* HERE IMPLEMENT SOMETHING: I STILL DON'T KNOW WHAT */
-                })
                 .onComplete(asyncResult -> {
                     //System.out.println("Promise completed with: " + asyncResult.result());
                     travels.complete(
@@ -78,6 +65,25 @@ public class TrainAPIWebClient {
                 });
 
         return travels.future();
+    }
+
+
+    /**
+     *
+     * Method that retrieves real time information about a single train.
+     *
+     * @param stationName
+     * @param trainCode
+     * @return
+     */
+    public Future<Train> getRealTimeTrainInfo(String stationName, String trainCode) {
+        try {
+            Promise<JsonObject> jsonResponse = this.requests.getRequestForRealTimeTrainInfo(stationName, trainCode, System.currentTimeMillis());
+            return futureRealTimeTrainInfo(jsonResponse);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private Future<Train> futureRealTimeTrainInfo(Promise<JsonObject> jsonPromise) {
@@ -102,6 +108,24 @@ public class TrainAPIWebClient {
         return trainStatus.future();
     }
 
+    /**
+     *
+     * Method that retrieves real time information about a single station.
+     *
+     * @param stationName
+     * @param arrivalsOrDepartures
+     * @return
+     */
+    public Future<Station> getRealTimeStationInfo(String stationName, StationStatus.ArrivalsOrDepartures arrivalsOrDepartures) {
+        try {
+            Promise<JsonArray> jsonResponse = this.requests.getRequestForRealTimeStationInfo(stationName, arrivalsOrDepartures, Calendar.getInstance().getTime());
+            return futureRealTimeStationInfo(jsonResponse, arrivalsOrDepartures);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     private Future<Station> futureRealTimeStationInfo(Promise<JsonArray> jsonPromise, StationStatus.ArrivalsOrDepartures arrivalsOrDepartures) {
         Promise<Station> stationStatus = Promise.promise();
         jsonPromise
@@ -109,10 +133,6 @@ public class TrainAPIWebClient {
                 .onFailure(err -> {
                     System.out.println("Something went wrong in JSON response: " + err.getMessage());
                     stationStatus.fail(err.getMessage());
-                })
-                .onSuccess(json -> {
-                    //System.out.println("Received a json array: " + json);
-                    /* HERE IMPLEMENT SOMETHING: I STILL DON'T KNOW WHAT */
                 })
                 .onComplete(asyncResult -> {
                     //System.out.println("Promise completed with: " + asyncResult.result());
